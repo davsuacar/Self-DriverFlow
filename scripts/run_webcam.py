f818aceb2d355d9b35a997661e2e4e1edede9042
Inno Team <davsuacar@gmail.com>
2017-08-31 11:41:41 +0200

    create dataset script

--- scripts/run_webcam.py
+++ scripts/run_webcam.py
@@ -2,27 +2,41 @@ import cv2
 import rospy
 from geometry_msgs.msg import Twist
 from time import sleep
+import csv
     
-def callback(data):
-	rospy.loginfo("I heard %s",data)
-
-def listener():
-	rospy.init_node('node_name2')
-	rospy.Subscriber("/mobile_base/commands/velocity", Twist, callback)
-
-cv2.namedWindow("preview")
+i = 0
 vc = cv2.VideoCapture(0)
 
+def callback(data):
+	print("callback called...")
+	global i
+	global vc
+	rospy.loginfo("I heard %s",data)
+	with open('dataset.csv', 'a') as csvfile:
+	    spamwriter = csv.writer(csvfile, delimiter=' ',
+		                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	    spamwriter.writerow([data.linear.z])
+
+	    if vc.isOpened(): # try to get the first frame
+	        rval, frame = vc.read()
+            else:
+		rval = False
+
+
+	    rval, frame = vc.read()
+	    cv2.imwrite("images/image_" + str(i) + ".png", frame)
+	    i += 1 
+
+def listener():
+	rospy.init_node('node_name3')
+	rospy.Subscriber("/mobile_base/commands/velocity", Twist, callback)
+
+
+
 listener()
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
+print("listener initialized...")
 
-else:
-    rval = False
-
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read() 
+while True:
     
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
--- scripts/take_photo.py
+++ scripts/take_photo.py
@@ -6,9 +6,10 @@ import scipy.ndimage
 video_capture = cv2.VideoCapture(0)
 print(video_capture)
 
+i = 0
 for i in range(3):
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-    print(frame)
-
+    cv2.imwrite("image_" + str(i) + ".png", frame)
+    i += 1
 video_capture.release()
