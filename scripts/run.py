d3797d377977c7d097dee860eae307af5f36d608
David <davsuacar@gmail.com>
2017-10-04 12:51:05 +0200

    feat: topic changed

--- scripts/run.py
+++ scripts/run.py
@@ -114,12 +114,12 @@ rospy.loginfo("To stop TurtleBot CTRL + C")
 
 # Create a publisher which can "talk" to TurtleBot and tell it to move
 # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+cmd_vel = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
 
 # TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
 r = rospy.Rate(10)
 
-for i in range(10):
+while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
@@ -134,7 +134,13 @@ for i in range(10):
     # let's go forward at 0.2 m/s
     move_cmd.linear.x = 0.1
     # let's turn at 0 radians/s
-    move_cmd.angular.z = 5
+    action = np.argmax(predict)
+    if action == 0:
+      move_cmd.angular.z = 0.0
+    elif action == 1:
+      move_cmd.angular.z = 0.1
+    else:
+      move_cmd.angular.z = -0.1
 
     cmd_vel.publish(move_cmd)
 
